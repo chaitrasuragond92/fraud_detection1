1bddcc675cca58c86228f0d89485bd88ff101627
chaitrasuragond92 <chaitra.shivanand19@gmail.com>
2025-12-16 15:53:13 +0530

    adding data processing python code

---
+++ dataprocessing.py
@@ -0,0 +1,16 @@
+
+print ("adding all the data processing code related to fraud detection")
+
+a = 5
+b = 7
+def sum(a,b):
+   c = a+b
+   return c
+
+print("sum function")
+
+def multiplication(a,b):
+   d = a+b
+   return b
+
+print("multiplication function")
